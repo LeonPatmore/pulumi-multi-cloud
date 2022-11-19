97a1841906e5dfcb54f8ce3157f9f55f5910cd2e
Leon Patmore <leon.patmore@bath.edu>
2022-11-19 02:51:59 +0000

    improving the example.

--- pulumi_multi_cloud/common.py
+++ pulumi_multi_cloud/common.py
@@ -1,4 +1,5 @@
 import enum
+import logging
 
 from pulumi_multi_cloud.aws.common import AwsCloudResource
 from pulumi_multi_cloud.gcp.common import GcpCloudResource
@@ -14,6 +15,10 @@ class CloudProvider(enum.Enum):
 
     AWS = "aws"
     GCP = "gcp"
+
+
+DEFAULT_REGION = CloudRegion.EU
+DEFAULT_PROVIDER = CloudProvider.AWS
 
 
 class MultiCloudResourceType:
@@ -36,27 +41,26 @@ class MultiCloudResourceType:
 
 class MultiCloudResourceFactory:
 
-    def __init__(self, default_region: CloudRegion, default_provider: CloudProvider):
-        self.default_region = default_region
-        self.default_provider = default_provider
+    def __init__(self,
+                 region: CloudRegion = DEFAULT_REGION,
+                 provider: CloudProvider = DEFAULT_PROVIDER):
+        self.region = region
+        self.provider = provider
 
     def create(self,
                resource_type: type(MultiCloudResourceType),
                name: str,
-               provider: CloudProvider = None,
-               region: CloudRegion = None,
                **kwargs):
-        if provider is None:
-            provider = self.default_provider
-        if region is None:
-            region = self.default_region
-
-        p_class = resource_type.gcp_type if provider == CloudProvider.GCP else resource_type.aws_type
-        resource_factory = resource_type(name=name, region=region, **kwargs)
-        props = resource_factory.gcp_kwargs if provider == CloudProvider.GCP else resource_factory.aws_kwargs
+        p_class = resource_type.gcp_type if self.provider == CloudProvider.GCP else resource_type.aws_type
+        if p_class is None:
+            logging.info(f"Skipping this resource [ {name} ] of type [ {resource_type} ] "
+                         f"because there no abstraction for provider [ {self.provider.name} ]")
+            return None
+        resource_factory = resource_type(name=name, region=self.region, **kwargs)
+        props = resource_factory.gcp_kwargs if self.provider == CloudProvider.GCP else resource_factory.aws_kwargs
 
         pulumi_resource = p_class(resource_name=name, **props())
         pulumi_resource.__class__ = resource_type.gcp_multi_cloud_type \
-            if provider == CloudProvider.GCP \
+            if self.provider == CloudProvider.GCP \
             else resource_type.aws_multi_cloud_type
         return pulumi_resource
--- s3_multi_cloud_example.py
+++ s3_multi_cloud_example.py
@@ -6,20 +6,21 @@ from pulumi_multi_cloud.resources.bucket import MultiCloudBucketType
 from pulumi_multi_cloud.resources.function import FunctionRuntime, MultiCloudFunctionType
 from pulumi_multi_cloud.resources.permissions import MultiCloudPermissionsType
 
-generator = MultiCloudResourceFactory(default_region=CloudRegion.EU, default_provider=CloudProvider.AWS)
-
-
-aws_bucket = generator.create(MultiCloudBucketType, "leon-multicloud")
-gcp_bucket = generator.create(MultiCloudBucketType, "leon-multicloud", provider=CloudProvider.GCP)
-
 code = AssetArchive({".": FileArchive("./example")})
-permissions = generator.create(MultiCloudPermissionsType, name="function-permissions")
 
-aws_function = generator.create(MultiCloudFunctionType, "leon-function", runtime=FunctionRuntime.Python39, files=code, permissions=permissions)
 
-gcp_function = generator.create(MultiCloudFunctionType, "leon-function", runtime=FunctionRuntime.Python39, files=code, permissions=permissions, provider=CloudProvider.GCP)
+def my_cool_exposed_function(gen: MultiCloudResourceFactory):
+    bucket = gen.create(MultiCloudBucketType, "leon-multicloud")
+    permissions = gen.create(MultiCloudPermissionsType, name="function-permissions")
+    function = gen.create(MultiCloudFunctionType,
+                          "leon-function",
+                          runtime=FunctionRuntime.Python39,
+                          files=code,
+                          permissions=permissions)
+    pulumi.export(f"bucket_id_{gen.provider.name}", bucket.get_id())
+    pulumi.export(f"function_id_{gen.provider.name}", function.get_id())
 
-pulumi.export("aws_bucket_id", aws_bucket.get_id())
-pulumi.export("gcp_bucket_id", gcp_bucket.get_id())
-pulumi.export("aws_function_id", aws_function.get_id())
-pulumi.export("gcp_function_id", gcp_function.get_id())
+
+my_cool_exposed_function(MultiCloudResourceFactory(region=CloudRegion.EU, provider=CloudProvider.AWS))
+my_cool_exposed_function(MultiCloudResourceFactory(region=CloudRegion.EU, provider=CloudProvider.GCP))
+
