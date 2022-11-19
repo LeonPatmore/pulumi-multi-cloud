e75b5858b6e19906aec507c66507d6308170c884
Leon Patmore <leon.patmore@bath.edu>
2022-11-19 15:24:02 +0000

    Fixing.

--- pulumi_multi_cloud/aws/function.py
+++ pulumi_multi_cloud/aws/function.py
@@ -1,3 +1,9 @@
+import json
+
+import pulumi_aws
+from pulumi import ResourceOptions
+from pulumi_aws import apigateway, lambda_
+from pulumi_aws.apigateway import RestApi
 from pulumi_aws.lambda_ import Function
 
 from pulumi_multi_cloud.aws.common import AwsCloudResource
@@ -11,10 +17,56 @@ AWS_RUNTIME = {
 
 class AwsFunctionGenerator(ProviderFunctionResourceGenerator):
 
+    @staticmethod
+    def _generate_spec(arn: str):
+        lambda_uri = f"arn:aws:apigateway:{pulumi_aws.get_region().id}:lambda:path/2015-03-31/functions/{arn}/invocations"
+        return {
+            "swagger": "2.0",
+            "info": {"title": "api", "version": "1.0"},
+            "paths": {
+                "/{proxy+}": {
+                    "x-amazon-apigateway-any-method": {
+                        "x-amazon-apigateway-integration": {
+                            "uri": lambda_uri,
+                            "passthroughBehavior": "when_no_match",
+                            "type": "aws_proxy",
+                            "httpMethod": "POST"
+                        }
+                    }
+                }
+            }
+        }
+
+    def _expose_http(self, func: Function) -> tuple:
+
+        def get_execution_arn(arn: str):
+            gw_id = arn.split("/")[-1]
+            return f"arn:aws:execute-api:{pulumi_aws.get_region().id}:{pulumi_aws.get_caller_identity().account_id}:{gw_id}/*/*/*"
+
+        api = RestApi(f"{self.name}-api",
+                      body=func.arn.apply(lambda arn: json.dumps(self._generate_spec(arn))))
+        deployment = apigateway.Deployment("api-deployment",
+                                           rest_api=api.id,
+                                           stage_name="Prod",
+                                           opts=ResourceOptions(depends_on=[api]))
+        invoke_permission = lambda_.Permission("api-lambda-permission",
+                                               action="lambda:invokeFunction",
+                                               function=func.name,
+                                               principal="apigateway.amazonaws.com",
+                                               source_arn=api.arn.apply(get_execution_arn),
+                                               opts=ResourceOptions(depends_on=[api]))
+        return api, deployment, invoke_permission
+
     def generate_resources(self) -> MultiCloudResourceCreation:
         function = Function(self.name,
                             code=self.files,
                             runtime=AWS_RUNTIME[self.runtime],
                             handler=f"{self.function_handler.file}.{self.function_handler.method}",
                             role=self.permissions.arn)
-        return MultiCloudResourceCreation(AwsCloudResource.given(function))
+        creation = MultiCloudResourceCreation(AwsCloudResource.given(function))
+        if self.http_trigger:
+            api, deployment, invoke_permission = self._expose_http(function)
+            creation.with_resource(AwsCloudResource.given(api))\
+                .with_resource(AwsCloudResource.given(deployment))\
+                .with_resource(AwsCloudResource.given(invoke_permission))
+        return creation
--- s3_multi_cloud_example.py
+++ s3_multi_cloud_example.py
@@ -2,7 +2,7 @@ import pulumi
 from pulumi import AssetArchive, FileArchive
 
 from pulumi_multi_cloud.common import CloudProvider, CloudRegion, MultiCloudResourceFactory
-from pulumi_multi_cloud.resources.function import FunctionHandler, FunctionRuntime
+from pulumi_multi_cloud.resources.function import FunctionHandler, FunctionRuntime, FunctionHttpTrigger
 from pulumi_multi_cloud.resources.types import DefaultTypes
 
 code = AssetArchive({".": FileArchive("./example")})
@@ -12,12 +12,14 @@ def my_cool_exposed_function(gen: MultiCloudResourceFactory):
     bucket = gen.create(DefaultTypes.Bucket.value, "leon-multicloud").main_resource
     permissions = gen.create(DefaultTypes.Permissions.value, name="function-permissions").main_resource
     function_handler = FunctionHandler(method="handle_aws" if gen.provider == CloudProvider.AWS else "handle_gcp")
+    function_trigger = FunctionHttpTrigger(public=True)
     function = gen.create(DefaultTypes.Function.value,
                           "leon-function",
                           runtime=FunctionRuntime.Python39,
                           files=code,
                           function_handler=function_handler,
-                          permissions=permissions).main_resource
+                          permissions=permissions,
+                          http_trigger=function_trigger).main_resource
 
     pulumi.export(f"bucket_id_{gen.provider.name}", bucket.get_id())
     pulumi.export(f"function_id_{gen.provider.name}", function.get_id())
